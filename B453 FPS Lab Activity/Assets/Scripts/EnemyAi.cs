011053b1ee7bf9974bfcfd3822084d7f0f63f867
Deturne <159824448+Deturne@users.noreply.github.com>
2025-02-27 19:39:46 -0500

    Finished Code

--- B453 FPS Lab Activity/Assets/Scripts/EnemyAi.cs
+++ B453 FPS Lab Activity/Assets/Scripts/EnemyAi.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class EnemyAi : MonoBehaviour
+public class EnemyAi : MonoBehaviour,IDamagable
 {
     [SerializeField] float speed = 5f;
     [SerializeField] float stoppingDistance = 5f;
+    [SerializeField] GameObject[] waypoints;
 
     [SerializeField, Min(5)] float attackRange = 5f;
 
     private NavMeshAgent agent;
     private Transform player;
+    //Raycast hit;
 
     private void Awake()
     {
@@ -19,11 +22,13 @@ public class EnemyAi : MonoBehaviour
 
         agent.speed = speed;
         agent.stoppingDistance = stoppingDistance;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if(Vector3.Distance(transform.position, player.position) <= agent.stoppingDistance)
         {
             Attack();
@@ -32,11 +37,28 @@ public class EnemyAi : MonoBehaviour
         {
             Move();
         }
+
+        
     }
 
     private void Move()
     {
-        agent.SetDestination(player.position);
+        RaycastHit hit;
+
+       
+        
+        Debug.DrawRay(transform.position, transform.forward * attackRange, Color.blue, .5f);
+       if(Physics.Raycast(transform.position,transform.forward, out hit, attackRange)){
+
+            if(hit.collider.CompareTag("Player")){
+            agent.SetDestination(player.position);
+            }
+            else{
+                
+                agent.SetDestination(waypoints[0].transform.position);
+                StartCoroutine(wait());
+            }
+       }
     }
 
     private void Attack()
@@ -44,7 +66,7 @@ public class EnemyAi : MonoBehaviour
         transform.LookAt(player);
 
         RaycastHit hit;
-        Debug.DrawRay(transform.position, transform.forward * attackRange, Color.magenta, 0.1f);
+        Debug.DrawRay(transform.position, transform.forward * attackRange, Color.magenta, 6f);
 
         if(Physics.Raycast(transform.position,transform.forward, out hit, attackRange))
         {
@@ -53,5 +75,18 @@ public class EnemyAi : MonoBehaviour
                 Debug.Log("Haha");
             }
         }
+    }
+
+    protected IEnumerator wait(){
+        
+        yield return new WaitForSeconds(6f);
+        agent.SetDestination(waypoints[1].transform.position);
+
+    }
+
+    public void TakeDamage(int damage){
+    
+        agent.SetDestination(player.position);
+        
     }
 }
